a7d4c3afd1cb0134b570748115ad49ab0bd144bc
datlt2306 <datlt2306@gmail.com>
2025-09-15 14:31:59 +0700

    feat(search): cải thiện chức năng tìm kiếm cho danh sách sản phẩm
    - Tối ưu hóa thuật toán tìm kiếm để nâng cao tốc độ phản hồi.
    - Cập nhật giao diện người dùng với các bộ lọc tìm kiếm mới.
    - Sửa lỗi hiển thị kết quả tìm kiếm không chính xác trước đó.

---
+++ chapter-01/variable.js
@@ -0,0 +1,39 @@
+// var, let, const
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+// let b = 10;
+// let b = 20;
+// console.log(b);
+
+// const c = 10;
+// console.log(c);
+
+// const myName = "Đạt";
+// const myAge = 36;
+// const myStatus = true;
+
+// const info = {
+//     name: "Đạt",
+//     age: 36,
+//     status: true,
+// };
+// const myFriends = ["Huy", "Hải", "Huyền", "Hằng"];
+
+// console.log(info);
+
+// Hàm
+
+// function name
+console.log(sum(10, 20));
+function sum(a, b) {
+    return a + b;
+}
+
+// function expression
+console.log(sum2(10, 20));
+const sum2 = function (a, b) {
+    return a + b;
+};

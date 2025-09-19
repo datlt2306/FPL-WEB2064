ee56211520ea93c823b07ceb617802f1cfaf2e50
datlt2306 <datlt2306@gmail.com>
2025-09-19 13:33:39 +0700

    feat(main): cập nhật ví dụ sử dụng các phương thức xử lý mảng
    - Thêm các ví dụ về cách sử dụng các phương thức như for...in, for...of, forEach, map và filter.
    - Cập nhật logic để lọc và hiển thị danh sách bạn bè, loại bỏ "Huy" khỏi danh sách.
    - Cải thiện khả năng hiển thị thông tin mảng cũ và mảng mới trong console.

--- chapter-03/main.js
+++ chapter-03/main.js
@@ -1,9 +1,44 @@
 const myFriends = ["Dat", "Thang", "Huy", "Hieu", "Long"];
+//                 a
 const listElement = document.getElementById("listElement");
 
 let content = "";
 // for
-for (let index = 0; index < myFriends.length; index++) {
-    content = content + `<li>${myFriends[index]}</li>`;
-}
-listElement.innerHTML = content;
+// for (let index = 0; index < myFriends.length; index++) {
+//     content = content + `<li>${myFriends[index]}</li>`;
+// }
+
+// for...in
+// for (let index in myFriends) {
+//     content = content + `<li>${myFriends[index]}</li>`;
+// }
+
+// for...of
+// for (let friend of myFriends) {
+//     content = content + `<li>${friend}</li>`;
+// }
+
+// forEach
+// syntax: array.forEach(function(item, index)){}
+// myFriends.forEach((friend, index) => {
+//     content = content + `<li>${friend} -  ${index}</li>`;
+// });
+
+// map, filter, find, reduce, every, some
+
+// map
+// syntax: array.map(function(item, index)){}
+// const newFriends = myFriends
+//     .map((friend) => {
+//         return `<li>${friend}</li>`;
+//     })
+//     .join("");
+// filter
+const newFriends = myFriends
+    .filter((friend) => friend !== "Huy")
+    .map((friend) => `<li>${friend}</li>`)
+    .join("");
+
+console.log("Mảng cũ", myFriends);
+console.log("Mảng mới", newFriends);
+listElement.innerHTML = newFriends;

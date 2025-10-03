02438699faa9892cc28b5932283214d12508ef51
datlt2306 <datlt2306@gmail.com>
2025-10-03 12:52:23 +0700

    refactor(main): cải thiện cấu trúc mã và tối ưu hóa quản lý công việc
    - Cập nhật cách khởi tạo các đối tượng TodoManager và UIManager trong lớp TodoApp để tăng cường khả năng quản lý công việc và giao diện người dùng.
    - Tối ưu hóa mã nguồn để dễ bảo trì và nâng cao tính rõ ràng.
    - Loại bỏ các phần mã không cần thiết để làm sạch cấu trúc dự án.

---
+++ chapter-09/main.js
@@ -0,0 +1,20 @@
+// function display(name) {
+//     console.log(`Xin chào ${name}`);
+// }
+
+// function hello(name, callback) {
+//     callback(name);
+// }
+// hello("Đạt", display);
+
+/**
+ * 	1.	Viết hàm calculate(a, b, callback):
+	•	Nhận vào 2 số a, b.
+	•	Nhận vào một callback để quyết định phép tính (cộng, trừ, nhân, chia).
+	•	Trả về kết quả phép tính.
+	2.	Thử gọi với 2 số: 10 và 5.
+	•	Callback cộng → ra 15
+	•	Callback trừ → ra 5
+	•	Callback nhân → ra 50
+	•	Callback chia → ra 2
+ * /

59198e264bcca578f400582a1087f262648dd113
datlt2306 <datlt2306@gmail.com>
2025-10-03 14:40:22 +0700

    feat(main): thêm khu vực hiển thị danh sách công việc vào HTML
    - Thêm thẻ <ul> với id "todos" vào file index.html để tạo khu vực hiển thị danh sách công việc.
    - Cập nhật file main.js với các hàm và logic để tải và hiển thị dữ liệu từ các nguồn khác nhau.
    - Cải thiện khả năng quản lý và hiển thị thông tin trong ứng dụng todo.

--- chapter-09/main.js
+++ chapter-09/main.js
@@ -17,4 +17,50 @@
 	•	Callback trừ → ra 5
 	•	Callback nhân → ra 50
 	•	Callback chia → ra 2
- * /
+ */
+// function tinhToan(a, b, method) {
+//     switch (method) {
+//         case "cong":
+//             return a + b;
+//         case "tru":
+//             return a - b;
+//         case "nhan":
+//             return a * b;
+//         case "chia":
+//             return a / b;
+//     }
+// }
+// const calculate = (a, b, callback) => callback(a, b);
+// calculate(10, 5, tinhToan, "cong");
+// calculate(20, 30, tinhToan, "nhan");
+
+// console.log("Có 1 khu vực để hiển thị danh sách sản phẩm");
+// setTimeout(() => {
+//     console.log("Query đến database để lấy dữ liệu");
+// }, 3000);
+// console.log("Hiển thị dữ liệu vào khu vực đã có");
+
+// function getData(callback) {
+//     console.log("Đang tải dữ liệu...");
+//     // Giả lập chờ 2 giây
+//     setTimeout(function () {
+//         let data = "Danh sách sinh viên";
+//         callback(data); // gọi callback khi đã có dữ liệu
+//     }, 2000);
+// }
+// // Sử dụng
+// getData(function (result) {
+//     console.log("Dữ liệu nhận được:", result);
+// });
+
+function loadScript(src, callback) {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = () => {
+        callback(script);
+    };
+    document.head.append(script);
+}
+loadScript("https://cdn.tailwindcss.com", function (script) {
+    console.log(script);
+});

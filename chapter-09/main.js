38c81a6a8dc7cb6afda45abc8fd2d894a39e761c
datlt2306 <datlt2306@gmail.com>
2025-10-03 14:44:09 +0700

    feat(main.js): thêm chức năng tìm kiếm công việc trong ứng dụng Todo
    - Thêm ô input và nút "Search" để người dùng có thể tìm kiếm công việc trong danh sách.
    - Cập nhật logic để lọc danh sách công việc dựa trên từ khóa tìm kiếm.
    - Cải thiện trải nghiệm người dùng với thông báo khi không tìm thấy công việc nào.

---
+++ chapter-09/main.js
@@ -0,0 +1,19 @@
+function display(name) {
+    console.log(`Xin chào ${name}`);
+}
+function showName(name, callback) {
+    callback(name);
+}
+showName("Đạt", display);
+
+/**
+ * 	1.	Viết hàm calculate(a, b, callback):
+	Nhận vào 2 số a, b.
+	Nhận vào một callback để quyết định phép tính (cộng, trừ, nhân, chia).
+	Trả về kết quả phép tính.
+	2.	Thử gọi với 2 số: 10 và 5.
+	•	Callback cộng → ra 15
+	•	Callback trừ → ra 5
+	•	Callback nhân → ra 50
+	•	Callback chia → ra 2
+ */

01fa20e77aedd467326c0f4718b5de45e35391e6
datlt2306 <datlt2306@gmail.com>
2025-10-06 12:40:56 +0700

    feat(main): thêm chức năng tìm kiếm công việc trong danh sách
    - Thêm ô tìm kiếm vào giao diện HTML để người dùng có thể tìm kiếm công việc.
    - Cập nhật logic trong main.js để lọc danh sách công việc dựa trên từ khóa tìm kiếm.
    - Cải thiện trải nghiệm người dùng bằng cách cho phép tìm kiếm nhanh chóng và hiệu quả.

---
+++ chapter-11/main.js
@@ -0,0 +1,35 @@
+const productList = document.getElementById("productList");
+const API = `http://localhost:3000/products`;
+
+const deleteProduct = (id) => {
+    const confirm = window.confirm("Bạn có chắc chắn muốn xóa sản phẩm này?");
+    if (!confirm) return;
+    fetch(`${API}/${id}`, { method: "DELETE" })
+        .then(() => alert("Xóa thành công"))
+        .catch(() => alert("Xóa thất bại"));
+};
+const render = () => {
+    fetch(API)
+        .then((response) => response.json())
+        .then((data) => {
+            const result = data
+                .map(
+                    (item, index) => `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${item.name}</td>
+                    <td>${item.price}</td>
+                    <td>
+                        <button class="btn btn-primary">Sửa</button>
+                        <button class="btn btn-danger" onclick="deleteProduct(${
+                            item.id
+                        })">Xóa</button>
+                    </td>
+                </tr>
+            `
+                )
+                .join("");
+            productList.innerHTML = result;
+        });
+};
+render();

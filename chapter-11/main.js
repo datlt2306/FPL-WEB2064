afb0acec42da9cdb71018e1712610a8ab727b83b
datlt2306 <datlt2306@gmail.com>
2025-10-08 15:48:08 +0700

    feat(edit.html, main.js, db.json): implement product editing feature and update product list
    - Enhanced edit.html to include a Bootstrap-styled form for updating product details.
    - Updated main.js to handle fetching product data for editing and submitting updates via a PUT request.
    - Added a sample product to db.json for testing purposes.
    - Included axios library in add.html, edit.html, and index.html for API requests.

--- chapter-11/main.js
+++ chapter-11/main.js
@@ -1,29 +1,59 @@
 const API = `http://localhost:3001/products`;
+const axios = window.axios;
 const productList = document.getElementById("productList");
 const productAddForm = document.getElementById("productAddForm");
+const productEditForm = document.getElementById("productEditForm");
 
+const idProduct = new URLSearchParams(window.location.search).get("id");
+if (idProduct) {
+    // lấy id từ url và call API lấy dữ liệu sản phẩm theo id
+    axios.get(`${API}/${idProduct}`).then((data) => {
+        // đổ dữ liệu vào form
+        document.getElementById("name").value = data.name;
+        document.getElementById("price").value = data.price;
+        document.getElementById("quantity").value = data.quantity;
+        document.getElementById("category").value = data.category;
+        document.getElementById("image").value = data.imageUrl;
+    });
+}
+if (productEditForm) {
+    productEditForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        updateProduct();
+    });
+}
 if (productAddForm) {
     productAddForm.addEventListener("submit", (e) => {
         e.preventDefault();
         addProduct();
     });
 }
+
+const updateProduct = () => {
+    axios
+        .put(`${API}/${idProduct}`, {
+            name: document.getElementById("name").value,
+            price: document.getElementById("price").value,
+            quantity: document.getElementById("quantity").value,
+            category: document.getElementById("category").value,
+            imageUrl: document.getElementById("image").value,
+        })
+        .then(() => {
+            console.log("Cập nhật sản phẩm thành công");
+            window.location.href = "./index.html";
+        })
+        .catch(() => console.log("Thất bại!"));
+};
 const addProduct = () => {
-    const product = {
-        name: document.getElementById("name").value,
-        price: document.getElementById("price").value,
-        quantity: document.getElementById("quantity").value,
-        category: document.getElementById("category").value,
-        imageUrl: document.getElementById("image").value,
-    };
     // call API thêm sản phẩm
-    fetch(`${API}`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product),
-    })
+    axios
+        .post(`${API}`, {
+            name: document.getElementById("name").value,
+            price: document.getElementById("price").value,
+            quantity: document.getElementById("quantity").value,
+            category: document.getElementById("category").value,
+            imageUrl: document.getElementById("image").value,
+        })
         .then(() => {
             console.log("Thêm sản phẩm thành công");
             window.location.href = "./index.html";
@@ -33,20 +63,19 @@ const addProduct = () => {
 const deleteProduct = (id) => {
     const confirm = window.confirm("Bạn có chắc chắn muốn xóa sản phẩm này không?");
     if (!confirm) return;
-    fetch(`${API}/${id}`, {
-        method: "DELETE",
-    })
+    axios
+        .delete(`${API}/${id}`)
         .then(() => console.log("Xóa sản phẩm thành công"))
         .catch(() => console.log("Thất bại!"));
 };
 const renderProduct = () => {
-    fetch(API)
-        .then((response) => response.json())
-        .then((data) => {
-            if (!productList) return;
-            productList.innerHTML = data
-                .map(
-                    (item, index) => `
+    // lấy danh sách sản phẩm
+
+    axios.get(API).then((response) => {
+        if (!productList) return;
+        productList.innerHTML = response.data
+            .map(
+                (item, index) => `
                 <tr>
                     <td>${index + 1}</td>
                     <td><img width="50" src="${item.imageUrl}" alt="${item.name}" /></td>
@@ -62,8 +91,8 @@ const renderProduct = () => {
                     </td>
                 </tr>
             `
-                )
-                .join("");
-        });
+            )
+            .join("");
+    });
 };
 renderProduct();

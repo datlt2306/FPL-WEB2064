001646aff1edb03d4f5a29ab493c0edbe2877055
datlt2306 <datlt2306@gmail.com>
2026-07-22 13:59:05 +0700

    promise

--- sync-async/main.js
+++ sync-async/main.js
@@ -1,18 +1,3 @@
-function getUser(callback) {
-    setTimeout(function(){
-        callback({
-                id: 1,
-                name: "Nguyễn Văn An",
-                email: "an@gmail.com"
-        })
-    }, 2000);
-}
-console.log("Bắt đầu lấy thông tin user");
-getUser(function(user){
-    console.log(user)
-    console.log("Chương trình kết thúc");
-});
-// Bất đồng bộ
 // function getUser(callback) {
 //     setTimeout(() => {
 //         const user = { id: 1, name: "An" };
@@ -20,17 +5,70 @@ getUser(function(user){
 //     }, 2000);
 // }
 
+// function getOrders(userId, callback) {
+//     setTimeout(() => {
+//         const orders = [
+//             { id: 101, userId: userId },
+//             { id: 102, userId: userId }
+//         ];
+
+//         callback(orders);
+//     }, 2000);
+// }
+
+// function getOrderDetail(orderId, callback) {
+//     setTimeout(() => {
+//         const orderDetail = {
+//             id: orderId,
+//             product: "Laptop",
+//             price: 20000000
+//         };
+
+//         callback(orderDetail);
+//     }, 2000);
+// }
+// // callback hell
 // getUser((user) => {
-//     console.log(user);
+//     console.log("User:", user); // { id: 1, name: "An" };
+//     getOrders(user.id, (orders) => {
+//         console.log("Orders:", orders);
+//         getOrderDetail(orders[0].id, (orderDetail) => {
+//             console.log("Order Detail:", orderDetail);
+//         });
+//     });
 // });
 
+// promise 
 
-// const showResult = (result) => {
-//     console.log(`Tổng là ${result}`);
+// const cauHon = () => {
+//     return new Promise((resolve, reject) => {
+//         const status = true;
+//         if(!status) reject('Lắc đầu');
+//         setTimeout(function(){
+//             resolve('Gật đầu');
+//         }, 3000)
+//     });
 // }
+// cauHon()
+// .then(result => {
+//     console.log(result);
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(result + 'xxx');
+//         }, 3000);
+//     });
+// })
+// .then(result => console.log(result))
+// .catch(error => console.log(error))
 
-// const totalPrice = (a, b, callback) => {
-//     callback(a+b);
-// }
 
-// totalPrice(10, 20, showResult);
+// api
+
+fetch(`http://letrongdat.com/products`)
+    .then(result => result.json())
+    .then(data => {
+        console.log('Mảng cũ', data);
+        console.log('Mảng mới', data.map(item => `<div>${item.name}</div>`).join(''));
+       document.querySelector('#products').innerHTML = data.map(item => `<div>${item.name}</div>`).join('')
+    })
+    .catch(error => console.log(error))
